31cd4996e79fa8abc8e3770c3d1cd62e726e8fba
Gary Faircloth <arkadianriver@gmail.com>
2021-05-27 19:10:09 -0700

    using caller

--- index.js
+++ index.js
@@ -2,32 +2,21 @@
 
 const extend = require('extend-shallow')
 const { Liquid } = require('liquidjs')
+const caller = require('caller');
+const path = require('path');
+
+const callingPath = path.dirname(caller());
 
 const engine = new Liquid({
-  root: `${__dirname}/../cpd-mddoc-metalsmith/layouts/` // TODO: don't hardcode here
+  root: `${callingPath}/layouts/`
 })
 
 exports.name = 'liquid'
 exports.outputFormat = 'html'
 
-// TODO: this either
+// TODO: don't hardcode this here
 engine.registerFilter('raise_error', e => console.log('ERROR: '+ e))
 
 exports.renderAsync = function (str, options, locals) {
   return engine.parseAndRender(str, extend({}, options, locals))
 }
-
-/**
- * Compile the template asyncronously.
- *
- * TODO: Fix tests for this https://github.com/jstransformers/jstransformer/issues/79
-exports.compileAsync = function (str, options) {
-  return new Promise(function (resolve, reject) {
-    engine.parse(str).then(function (template) {
-      template.fn = template.render
-      template.dependencies = []
-      resolve(template)
-    }).catch(reject)
-  })
-}
-*/
