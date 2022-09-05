6096798c4c7ff336fb31ee81ff4d69e7d1845e59
Gary Faircloth <arkadianriver@gmail.com>
2022-09-05 14:24:59 -0400

    allow for engineOptions

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 'use strict'
 
 const extend = require('extend-shallow')
-const { Liquid } = require('liquidjs')
+const { Liquid } = require('liquidjs');
+//const { isObject, isArray } = require('liquidjs/dist/util/underscore');
 const path = require('path');
 
 const referencingPath = Object.values(require.cache)[0].children[0].path;
 
-const engine = new Liquid({
-  root: [`${referencingPath}/layouts/`, `${process.cwd()}/layouts/`]
-})
-
 exports.name = 'liquid'
 exports.outputFormat = 'html'
 
-// TODO: don't hardcode this here
-engine.registerFilter('raise_error', e => console.log('ERROR: '+ e))
-
 exports.renderAsync = function (str, options, locals) {
-  return engine.parseAndRender(str, extend({}, options, locals))
+  const defaultRoot = `${referencingPath}/layouts/`;
+  const isOptionsObject = options !== null && !Array.isArray(options) && typeof options === "object";
+  if (isOptionsObject) {
+    if (options.root) {
+      if (Array.isArray(options.root)) {
+        options.root.push(defaultRoot);
+      } else {
+        options.root = [ defaultRoot, options.root ];
+      }
+    } else {
+      options.root = defaultRoot;
+    }
+  } else {
+    options = { root: defaultRoot };
+  }
+
+  const engine = new Liquid(options);
+  
+  // TODO: don't hardcode this here
+  engine.registerFilter('raise_error', e => console.log('ERROR: '+ e));
+
+  return engine.parseAndRender(str, extend({}, options, locals));
 }
